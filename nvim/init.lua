8668c7e5716b45b5d9b0f1426236db69ac0fb8e5
Alessio Bandiera <47356461+ph04@users.noreply.github.com>
2022-07-15 14:56:57 +0200

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,9 @@
 require("plugins")
 require("nvim-tree-config")
+
+vim.api.nvim_command('inoremap jj <Esc>')
+vim.api.nvim_command('set rnu!')
+
+vim.api.nvim_command('colorscheme gruvbox')
+
+vim.api.nvim_command('let g:airline_powerline_fonts = 1')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,10 +1,9 @@
 return require('packer').startup(function()
         use 'wbthomason/packer.nvim'
-
-	use {
-	  	 'kyazdani42/nvim-tree.lua',
-  		 requires = {
-    			'kyazdani42/nvim-web-devicons', -- optional, for file icons
-	  	 },
-	}
+	use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
+	use { 'neoclide/coc.nvim', branch = 'release' }
+	use 'vim-airline/vim-airline'
+	use 'vim-airline/vim-airline-themes'
+	use 'ap/vim-css-color'
+	use 'morhetz/gruvbox'
 end)
